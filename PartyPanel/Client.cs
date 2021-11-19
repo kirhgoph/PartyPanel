d685825b1d09a8b455c98b5111642874c954a3c6
kirhgoph <evgenybogatenkov@gmail.com>
2021-11-19 21:15:00 +0300

    работает только загрузка

--- PartyPanel/Client.cs
+++ PartyPanel/Client.cs
@@ -44,7 +44,7 @@ namespace PartyPanel
             try
             {
                 client = new Network.Client(10155);
-                client.PacketRecieved += Client_PacketRecieved;
+                //client.PacketRecieved += Client_PacketRecieved;
                 client.ServerDisconnected += Client_ServerDisconnected;
                 client.Start();
 
@@ -101,77 +101,77 @@ namespace PartyPanel
             else PPLogger.Debug("Skipped sending songs because there is no server connected");
         }
 
-        private void Client_PacketRecieved(Packet packet)
-        {
-            if (packet.Type == PacketType.PlaySong)
-            {
-                PlaySong playSong = packet.SpecificPacket as PlaySong;
-        
-                var desiredLevel = Plugin.masterLevelList.First(x => x.levelID == playSong.levelId);
-                var desiredCharacteristic = Loader.beatmapCharacteristicCollection.beatmapCharacteristics.First(x => x.serializedName == playSong.ppCharacteristic.SerializedName);
-                //var desiredCharacteristic = Loader.beatmapCharacteristicCollection.beatmapCharacteristics.First(x => x.serializedName == playSong.ppCharacteristic.SerializedName);
-                var desiredDifficulty = (BeatmapDifficulty)playSong.difficulty;
-        
-                var playerSpecificSettings = new PlayerSpecificSettings();
-                // playerSpecificSettings.advancedHud = playSong.playerSettings.advancedHud;
-                // playerSpecificSettings.leftHanded = playSong.playerSettings.leftHanded;
-                // playerSpecificSettings.noTextsAndHuds = playSong.playerSettings.noTextsAndHuds;
-                // playerSpecificSettings.reduceDebris = playSong.playerSettings.reduceDebris;
-                //playerSpecificSettings.staticLights = playSong.playerSettings.staticLights;
-        
-                var gameplayModifiers = new GameplayModifiers();
-                // gameplayModifiers.batteryEnergy = playSong.gameplayModifiers.batteryEnergy;
-                // gameplayModifiers.disappearingArrows = playSong.gameplayModifiers.disappearingArrows;
-                // gameplayModifiers.failOnSaberClash = playSong.gameplayModifiers.failOnSaberClash;
-                // gameplayModifiers.fastNotes = playSong.gameplayModifiers.fastNotes;
-                // gameplayModifiers.ghostNotes = playSong.gameplayModifiers.ghostNotes;
-                // gameplayModifiers.instaFail = playSong.gameplayModifiers.instaFail;
-                // gameplayModifiers.noBombs = playSong.gameplayModifiers.noBombs;
-                // gameplayModifiers.noFail = playSong.gameplayModifiers.noFail;
-                // gameplayModifiers.noObstacles = playSong.gameplayModifiers.noObstacles;
-                // gameplayModifiers.songSpeed = (GameplayModifiers.SongSpeed)playSong.gameplayModifiers.songSpeed;
-        
-                SaberUtilities.PlaySong(desiredLevel, desiredCharacteristic, desiredDifficulty, gameplayModifiers, playerSpecificSettings);
-            }
-            // else if (packet.Type == PacketType.LoadSong)
-            // {
-            //     LoadSong loadSong = packet.SpecificPacket as LoadSong;
-            //
-            //     Action<IBeatmapLevel> SongLoaded = (loadedLevel) =>
-            //     {
-            //         var loadedSong = new LoadedSong();
-            //         var beatmapLevel = new PPPreviewBeatmapLevel();
-            //         beatmapLevel.Characteristics = loadedLevel.beatmapLevelData.difficultyBeatmapSets.Select(x => x.beatmapCharacteristic).Select(x => {
-            //             var characteristic = new PPCharacteristic();
-            //             characteristic.SerializedName = x.serializedName;
-            //             characteristic.difficulties =
-            //                 loadedLevel.beatmapLevelData.difficultyBeatmapSets
-            //                     .First(y => y.beatmapCharacteristic.serializedName == x.serializedName)
-            //                     .difficultyBeatmaps.Select(y => (PPCharacteristic.BeatmapDifficulty)y.difficulty).ToArray();
-            //
-            //             return characteristic;
-            //         }).ToArray();
-            //
-            //         beatmapLevel.LevelId = loadedLevel.levelID;
-            //         beatmapLevel.Name = loadedLevel.songName;
-            //         beatmapLevel.Loaded = true;
-            //
-            //         loadedSong.level = beatmapLevel;
-            //
-            //         client.Send(new Packet(loadedSong).ToBytes());
-            //     };
-            //
-            //     LoadSong(loadSong.levelId, SongLoaded);
-            // }
-            else if (packet.Type == PacketType.Command)
-            {
-                Command command = packet.SpecificPacket as Command;
-                if (command.commandType == Command.CommandType.ReturnToMenu)
-                {
-                    SaberUtilities.ReturnToMenu();
-                }
-            }
-        }
+        // private void Client_PacketRecieved(Packet packet)
+        // {
+        //     if (packet.Type == PacketType.PlaySong)
+        //     {
+        //         PlaySong playSong = packet.SpecificPacket as PlaySong;
+        //
+        //         var desiredLevel = Plugin.masterLevelList.First(x => x.levelID == playSong.levelId);
+        //         var desiredCharacteristic = Loader.beatmapCharacteristicCollection.beatmapCharacteristics.First(x => x.serializedName == playSong.ppCharacteristic.SerializedName);
+        //         //var desiredCharacteristic = Loader.beatmapCharacteristicCollection.beatmapCharacteristics.First(x => x.serializedName == playSong.ppCharacteristic.SerializedName);
+        //         var desiredDifficulty = (BeatmapDifficulty)playSong.difficulty;
+        //
+        //         var playerSpecificSettings = new PlayerSpecificSettings();
+        //         // playerSpecificSettings.advancedHud = playSong.playerSettings.advancedHud;
+        //         // playerSpecificSettings.leftHanded = playSong.playerSettings.leftHanded;
+        //         // playerSpecificSettings.noTextsAndHuds = playSong.playerSettings.noTextsAndHuds;
+        //         // playerSpecificSettings.reduceDebris = playSong.playerSettings.reduceDebris;
+        //         //playerSpecificSettings.staticLights = playSong.playerSettings.staticLights;
+        //
+        //         var gameplayModifiers = new GameplayModifiers();
+        //         // gameplayModifiers.batteryEnergy = playSong.gameplayModifiers.batteryEnergy;
+        //         // gameplayModifiers.disappearingArrows = playSong.gameplayModifiers.disappearingArrows;
+        //         // gameplayModifiers.failOnSaberClash = playSong.gameplayModifiers.failOnSaberClash;
+        //         // gameplayModifiers.fastNotes = playSong.gameplayModifiers.fastNotes;
+        //         // gameplayModifiers.ghostNotes = playSong.gameplayModifiers.ghostNotes;
+        //         // gameplayModifiers.instaFail = playSong.gameplayModifiers.instaFail;
+        //         // gameplayModifiers.noBombs = playSong.gameplayModifiers.noBombs;
+        //         // gameplayModifiers.noFail = playSong.gameplayModifiers.noFail;
+        //         // gameplayModifiers.noObstacles = playSong.gameplayModifiers.noObstacles;
+        //         // gameplayModifiers.songSpeed = (GameplayModifiers.SongSpeed)playSong.gameplayModifiers.songSpeed;
+        //
+        //         SaberUtilities.PlaySong(desiredLevel, desiredCharacteristic, desiredDifficulty, gameplayModifiers, playerSpecificSettings);
+        //     }
+        //     // else if (packet.Type == PacketType.LoadSong)
+        //     // {
+        //     //     LoadSong loadSong = packet.SpecificPacket as LoadSong;
+        //     //
+        //     //     Action<IBeatmapLevel> SongLoaded = (loadedLevel) =>
+        //     //     {
+        //     //         var loadedSong = new LoadedSong();
+        //     //         var beatmapLevel = new PPPreviewBeatmapLevel();
+        //     //         beatmapLevel.Characteristics = loadedLevel.beatmapLevelData.difficultyBeatmapSets.Select(x => x.beatmapCharacteristic).Select(x => {
+        //     //             var characteristic = new PPCharacteristic();
+        //     //             characteristic.SerializedName = x.serializedName;
+        //     //             characteristic.difficulties =
+        //     //                 loadedLevel.beatmapLevelData.difficultyBeatmapSets
+        //     //                     .First(y => y.beatmapCharacteristic.serializedName == x.serializedName)
+        //     //                     .difficultyBeatmaps.Select(y => (PPCharacteristic.BeatmapDifficulty)y.difficulty).ToArray();
+        //     //
+        //     //             return characteristic;
+        //     //         }).ToArray();
+        //     //
+        //     //         beatmapLevel.LevelId = loadedLevel.levelID;
+        //     //         beatmapLevel.Name = loadedLevel.songName;
+        //     //         beatmapLevel.Loaded = true;
+        //     //
+        //     //         loadedSong.level = beatmapLevel;
+        //     //
+        //     //         client.Send(new Packet(loadedSong).ToBytes());
+        //     //     };
+        //     //
+        //     //     LoadSong(loadSong.levelId, SongLoaded);
+        //     // }
+        //     else if (packet.Type == PacketType.Command)
+        //     {
+        //         Command command = packet.SpecificPacket as Command;
+        //         if (command.commandType == Command.CommandType.ReturnToMenu)
+        //         {
+        //             SaberUtilities.ReturnToMenu();
+        //         }
+        //     }
+        // }
 
         // private async void LoadSong(string levelId, Action<IBeatmapLevel> loadedCallback)
         // {
--- PartyPanel/Utilities/OstHelper.cs
+++ PartyPanel/Utilities/OstHelper.cs
@@ -3,173 +3,173 @@ using System.Linq;
 
 namespace PartyPanel.Utilities
 {
-    class OstHelper
-    {
-        public enum LevelDifficulty
-        {
-            Auto = -1,
-            Easy = 0,
-            Normal = 1,
-            Hard = 2,
-            Expert = 3,
-            ExpertPlus = 4
-        }
-
-        public enum BeatmapCharacteristic
-        {
-            Auto = -1,
-            Standard = 0,
-            OneSaber = 2,
-            NoArrows = 3
-        }
-
-        static OstHelper()
-        {
-            packs = new Pack[] {
-                new Pack
-                {
-                    PackID = "OstVol1",
-                    PackName = "Original Soundtrack Vol. 1",
-                    SongDictionary = new Dictionary<string, string>
-                    {
-                        { "BeatSaber", "Beat Saber" },
-                        { "Escape", "Escape" },
-                        { "LvlInsane", "Lvl Insane" },
-                        { "100Bills", "$100 Bills" },
-                        { "CountryRounds", "Country Rounds" },
-                        { "Breezer", "Breezer" },
-                        { "TurnMeOn", "Turn Me On" },
-                        { "BalearicPumping", "Balaeric Pumping" },
-                        { "Legend", "Legend" },
-                        { "CommercialPumping", "Commercial Pumping" }
-                    }
-                },
-                new Pack
-                {
-                    PackID = "OstVol2",
-                    PackName = "Original Soundtrack Vol. 2",
-                    SongDictionary = new Dictionary<string, string>
-                    {
-                        { "BeThereForYou", "Be There For You" },
-                        { "Elixia", "Elixia" },
-                        { "INeedYou", "I Need You" },
-                        { "RumNBass", "Rum n' Bass" },
-                        { "UnlimitedPower", "Unlimited Power" }
-                    }
-                },
-                new Pack
-                {
-                    PackID = "OstVol3",
-                    PackName = "Original Soundtrack Vol. 3",
-                    SongDictionary = new Dictionary<string, string>
-                    {
-                        { "Origins", "Origins" },
-                        { "ReasonForLiving", "Reason For Living" },
-                        { "GiveALittleLove", "Give a Little Love" },
-                        { "FullCharge", "Full Charge" },
-                        { "Immortal", "Immortal" },
-                        { "BurningSands", "Burning Sands" }
-                    }
-                },
-                new Pack
-                {
-                    PackID = "Extras",
-                    PackName = "Extras",
-                    SongDictionary = new Dictionary<string, string>
-                    {
-                        { "Crystallized", "Crystallized" },
-                        { "CycleHit", "Cycle Hit" },
-                        { "WhatTheCat", "WHAT THE CAT!?" },
-                        { "CrabRave", "Crab Rave" },
-                        { "AngelVoices", "Angel Voices" },
-                        { "OneHope", "One Hope" },
-                        { "PopStars", "POP/STARS - K/DA" }
-                    }
-                },
-                new Pack
-                {
-                    PackID = "Monstercat",
-                    PackName = "Monstercat Music Pack Vol. 1",
-                    SongDictionary = new Dictionary<string, string>
-                    {
-                        { "Boundless", "Boundless" },
-                        { "EmojiVIP", "Emoji VIP" },
-                        { "Epic", "EPIC" },
-                        { "FeelingStronger", "Feeling Stronger" },
-                        { "Overkill", "Overkill" },
-                        { "Rattlesnake", "Rattlesnake" },
-                        { "Stronger", "Stronger" },
-                        { "ThisTime", "This Time" },
-                        { "TillItsOver", "Till It's Over" },
-                        { "WeWontBeAlone", "We Won't Be Alone" },
-                    }
-                },
-                new Pack
-                {
-                    PackID = "ImagineDragons",
-                    PackName = "Imagine Dragons Music Pack",
-                    SongDictionary = new Dictionary<string, string>
-                    {
-                        { "BadLiar", "Bad Liar" },
-                        { "Believer", "Believer" },
-                        { "Digital", "Digital" },
-                        { "ItsTime", "It's Time" },
-                        { "Machine", "Machine" },
-                        { "Natural", "Natural" },
-                        { "Radioactive", "Radioactive" },
-                        { "Thunder", "Thunder" },
-                        { "Warriors", "Warriors" },
-                        { "WhateverItTakes", "Whatever It Takes" }
-                    }
-                }
-            };
-
-            foreach (Pack pack in packs)
-            {
-                allLevels = allLevels.Concat(pack.SongDictionary).ToDictionary(s => s.Key, s => s.Value);
-            }
-        }
-
-        public class Pack
-        {
-            public string PackID { get; set; }
-            public string PackName { get; set; }
-            public Dictionary<string, string> SongDictionary { get; set; }
-        }
-
-        public static readonly Pack[] packs;
-        public static readonly Dictionary<string, string> allLevels = new Dictionary<string, string>();
-
-        //C# doesn't seem to want me to use an array of a non-primitive here.
-        private static readonly int[] mainDifficulties = { (int)LevelDifficulty.Easy, (int)LevelDifficulty.Normal, (int)LevelDifficulty.Hard, (int)LevelDifficulty.Expert, (int)LevelDifficulty.ExpertPlus };
-        private static readonly int[] angelDifficulties = { (int)LevelDifficulty.Hard, (int)LevelDifficulty.Expert, (int)LevelDifficulty.ExpertPlus };
-        private static readonly int[] oneSaberDifficulties = { (int)LevelDifficulty.Expert };
-        private static readonly int[] noArrowsDifficulties = { (int)LevelDifficulty.Expert };
-
-        public static string GetOstSongNameFromLevelId(string hash)
-        {
-            hash = hash.EndsWith("OneSaber") ? hash.Substring(0, hash.IndexOf("OneSaber")) : hash;
-            hash = hash.EndsWith("NoArrows") ? hash.Substring(0, hash.IndexOf("NoArrows")) : hash;
-            return allLevels[hash];
-        }
-
-        public static LevelDifficulty[] GetDifficultiesFromLevelId(string levelId)
-        {
-            if (IsOst(levelId))
-            {
-                if (levelId.Contains("OneSaber")) return oneSaberDifficulties.Select(x => (LevelDifficulty)x).ToArray();
-                else if (levelId.Contains("NoArrows")) return noArrowsDifficulties.Select(x => (LevelDifficulty)x).ToArray();
-                else if (levelId != "AngelVoices") return mainDifficulties.Select(x => (LevelDifficulty)x).ToArray();
-                else return angelDifficulties.Select(x => (LevelDifficulty)x).ToArray();
-            }
-            return null;
-        }
-
-        public static bool IsOst(string levelId)
-        {
-            levelId = levelId.EndsWith("OneSaber") ? levelId.Substring(0, levelId.IndexOf("OneSaber")) : levelId;
-            levelId = levelId.EndsWith("NoArrows") ? levelId.Substring(0, levelId.IndexOf("NoArrows")) : levelId;
-            return packs.Any(x => x.SongDictionary.ContainsKey(levelId));
-        }
-    }
+    // class OstHelper
+    // {
+    //     public enum LevelDifficulty
+    //     {
+    //         Auto = -1,
+    //         Easy = 0,
+    //         Normal = 1,
+    //         Hard = 2,
+    //         Expert = 3,
+    //         ExpertPlus = 4
+    //     }
+    //
+    //     public enum BeatmapCharacteristic
+    //     {
+    //         Auto = -1,
+    //         Standard = 0,
+    //         OneSaber = 2,
+    //         NoArrows = 3
+    //     }
+    //
+    //     static OstHelper()
+    //     {
+    //         packs = new Pack[] {
+    //             new Pack
+    //             {
+    //                 PackID = "OstVol1",
+    //                 PackName = "Original Soundtrack Vol. 1",
+    //                 SongDictionary = new Dictionary<string, string>
+    //                 {
+    //                     { "BeatSaber", "Beat Saber" },
+    //                     { "Escape", "Escape" },
+    //                     { "LvlInsane", "Lvl Insane" },
+    //                     { "100Bills", "$100 Bills" },
+    //                     { "CountryRounds", "Country Rounds" },
+    //                     { "Breezer", "Breezer" },
+    //                     { "TurnMeOn", "Turn Me On" },
+    //                     { "BalearicPumping", "Balaeric Pumping" },
+    //                     { "Legend", "Legend" },
+    //                     { "CommercialPumping", "Commercial Pumping" }
+    //                 }
+    //             },
+    //             new Pack
+    //             {
+    //                 PackID = "OstVol2",
+    //                 PackName = "Original Soundtrack Vol. 2",
+    //                 SongDictionary = new Dictionary<string, string>
+    //                 {
+    //                     { "BeThereForYou", "Be There For You" },
+    //                     { "Elixia", "Elixia" },
+    //                     { "INeedYou", "I Need You" },
+    //                     { "RumNBass", "Rum n' Bass" },
+    //                     { "UnlimitedPower", "Unlimited Power" }
+    //                 }
+    //             },
+    //             new Pack
+    //             {
+    //                 PackID = "OstVol3",
+    //                 PackName = "Original Soundtrack Vol. 3",
+    //                 SongDictionary = new Dictionary<string, string>
+    //                 {
+    //                     { "Origins", "Origins" },
+    //                     { "ReasonForLiving", "Reason For Living" },
+    //                     { "GiveALittleLove", "Give a Little Love" },
+    //                     { "FullCharge", "Full Charge" },
+    //                     { "Immortal", "Immortal" },
+    //                     { "BurningSands", "Burning Sands" }
+    //                 }
+    //             },
+    //             new Pack
+    //             {
+    //                 PackID = "Extras",
+    //                 PackName = "Extras",
+    //                 SongDictionary = new Dictionary<string, string>
+    //                 {
+    //                     { "Crystallized", "Crystallized" },
+    //                     { "CycleHit", "Cycle Hit" },
+    //                     { "WhatTheCat", "WHAT THE CAT!?" },
+    //                     { "CrabRave", "Crab Rave" },
+    //                     { "AngelVoices", "Angel Voices" },
+    //                     { "OneHope", "One Hope" },
+    //                     { "PopStars", "POP/STARS - K/DA" }
+    //                 }
+    //             },
+    //             new Pack
+    //             {
+    //                 PackID = "Monstercat",
+    //                 PackName = "Monstercat Music Pack Vol. 1",
+    //                 SongDictionary = new Dictionary<string, string>
+    //                 {
+    //                     { "Boundless", "Boundless" },
+    //                     { "EmojiVIP", "Emoji VIP" },
+    //                     { "Epic", "EPIC" },
+    //                     { "FeelingStronger", "Feeling Stronger" },
+    //                     { "Overkill", "Overkill" },
+    //                     { "Rattlesnake", "Rattlesnake" },
+    //                     { "Stronger", "Stronger" },
+    //                     { "ThisTime", "This Time" },
+    //                     { "TillItsOver", "Till It's Over" },
+    //                     { "WeWontBeAlone", "We Won't Be Alone" },
+    //                 }
+    //             },
+    //             new Pack
+    //             {
+    //                 PackID = "ImagineDragons",
+    //                 PackName = "Imagine Dragons Music Pack",
+    //                 SongDictionary = new Dictionary<string, string>
+    //                 {
+    //                     { "BadLiar", "Bad Liar" },
+    //                     { "Believer", "Believer" },
+    //                     { "Digital", "Digital" },
+    //                     { "ItsTime", "It's Time" },
+    //                     { "Machine", "Machine" },
+    //                     { "Natural", "Natural" },
+    //                     { "Radioactive", "Radioactive" },
+    //                     { "Thunder", "Thunder" },
+    //                     { "Warriors", "Warriors" },
+    //                     { "WhateverItTakes", "Whatever It Takes" }
+    //                 }
+    //             }
+    //         };
+    //
+    //         foreach (Pack pack in packs)
+    //         {
+    //             allLevels = allLevels.Concat(pack.SongDictionary).ToDictionary(s => s.Key, s => s.Value);
+    //         }
+    //     }
+    //
+    //     public class Pack
+    //     {
+    //         public string PackID { get; set; }
+    //         public string PackName { get; set; }
+    //         public Dictionary<string, string> SongDictionary { get; set; }
+    //     }
+    //
+    //     public static readonly Pack[] packs;
+    //     public static readonly Dictionary<string, string> allLevels = new Dictionary<string, string>();
+    //
+    //     //C# doesn't seem to want me to use an array of a non-primitive here.
+    //     private static readonly int[] mainDifficulties = { (int)LevelDifficulty.Easy, (int)LevelDifficulty.Normal, (int)LevelDifficulty.Hard, (int)LevelDifficulty.Expert, (int)LevelDifficulty.ExpertPlus };
+    //     private static readonly int[] angelDifficulties = { (int)LevelDifficulty.Hard, (int)LevelDifficulty.Expert, (int)LevelDifficulty.ExpertPlus };
+    //     private static readonly int[] oneSaberDifficulties = { (int)LevelDifficulty.Expert };
+    //     private static readonly int[] noArrowsDifficulties = { (int)LevelDifficulty.Expert };
+    //
+    //     public static string GetOstSongNameFromLevelId(string hash)
+    //     {
+    //         hash = hash.EndsWith("OneSaber") ? hash.Substring(0, hash.IndexOf("OneSaber")) : hash;
+    //         hash = hash.EndsWith("NoArrows") ? hash.Substring(0, hash.IndexOf("NoArrows")) : hash;
+    //         return allLevels[hash];
+    //     }
+    //
+    //     public static LevelDifficulty[] GetDifficultiesFromLevelId(string levelId)
+    //     {
+    //         if (IsOst(levelId))
+    //         {
+    //             if (levelId.Contains("OneSaber")) return oneSaberDifficulties.Select(x => (LevelDifficulty)x).ToArray();
+    //             else if (levelId.Contains("NoArrows")) return noArrowsDifficulties.Select(x => (LevelDifficulty)x).ToArray();
+    //             else if (levelId != "AngelVoices") return mainDifficulties.Select(x => (LevelDifficulty)x).ToArray();
+    //             else return angelDifficulties.Select(x => (LevelDifficulty)x).ToArray();
+    //         }
+    //         return null;
+    //     }
+    //
+    //     public static bool IsOst(string levelId)
+    //     {
+    //         levelId = levelId.EndsWith("OneSaber") ? levelId.Substring(0, levelId.IndexOf("OneSaber")) : levelId;
+    //         levelId = levelId.EndsWith("NoArrows") ? levelId.Substring(0, levelId.IndexOf("NoArrows")) : levelId;
+    //         return packs.Any(x => x.SongDictionary.ContainsKey(levelId));
+    //     }
+    // }
 }
